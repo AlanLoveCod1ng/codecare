f21de6cef9271da473393e18420c3a044eb173ea
TIANCHENG <txiong53@wisc.edu>
2022-11-16 18:13:52 -0600

    update register function

--- Backend/application/routes.py
+++ Backend/application/routes.py
@@ -1,9 +1,14 @@
 import datetime
+import uuid
 from flask import redirect, request, jsonify, make_response
-from application import app, session
-from application.models import Account
+from application import app, session, db_engine, Base
+from application.models import Account, Patient, Provider
 from functools import wraps
+from sqlalchemy import func
 import jwt
+
+from sqlalchemy import Column, Integer, String
+
 
 def token_required(f):
     @wraps(f)
@@ -56,3 +61,51 @@ def login():
     else:
         return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login required"'})
 
+
+
+# class Patient(Base):
+#    __tablename__ = 'account'
+   
+#    __table_args__ = {'extend_existing': True} 
+   
+# #    account_id = Column(Integer, primary_key=True)
+# #    id = Column(Integer)
+#    id = Column(Integer, primary_key=True)
+#    is_patient = Column(Integer)
+#    first_name = Column(String)
+#    last_name = Column(String)
+#    phone = Column(String)
+#    email = Column(String)
+#    password = Column(String)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    data = request.form
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    phone = data['phone']
+    password = data['password']
+    confirm_password = data.get('confirm_password')
+
+
+
+    user = session.query(Account).filter(Account.email == email).first()
+
+    if not user:
+        # database ORM object
+        try:
+            id = session.query(func.max(Patient.patient_id)).first()[0]
+            newUser = Patient(patient_id = id+1, provider_id = None)
+            newAccount = Account(id = id, email = email, phone = phone, is_patient = 1, password = password, first_name = first_name, last_name = last_name)
+            session.add(newAccount)
+            session.add(newUser)
+            session.commit()
+        except:
+            return make_response("Can't register.", 401)
+        return make_response('Successfully registered.', 201)
+    else:
+        # returns 202 if user already exists
+        return make_response('User already exists. Please Log in.', 202)
+  
