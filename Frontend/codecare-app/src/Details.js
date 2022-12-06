2c36b90278b5681d8a49efd4d02c5e16f530fad0
Linksomanic <61527331+Linksomanic@users.noreply.github.com>
2022-12-06 03:49:28 -0600

    Massive Frontend changes (#14)
    Co-authored-by: Nishit Saraf <nishitsaraf@Nishits-MBP.lan>

--- Frontend/codecare-app/src/Details.js
+++ Frontend/codecare-app/src/Details.js
@@ -1,51 +1,15 @@
-import logo from './logo.svg';
-import CodecareIcon from './Codecare.png';
 import './App.css';
-import MapIcon from './map.JPG';
-import MessagesIcon from './messages.JPG';
-import NotificationIcon from './notifications.JPG';
-import OverviewIcon from './overview.JPG';
-import PatientsIcon from './patients.JPG';
-import SettingsIcon from './settings.JPG';
 import React, {useState, useEffect} from 'react';
 import Notification from './Notification';
-import {Route,Link, useLocation} from 'react-router-dom';
-
-function  getResponse(filter,first, token){
-    let notifications = [];
-    let response = null;
-    let apiData = null;
-     fetch('/notification?filter='+filter+"&first="+first+"&token="+token)
-     .then((response)=>response.json())
-     .then((apiData)=>{
-        
-       return apiData.json();
-        
-     })
-     .catch((error)=>{
-        //console.log("Hello");
-        
-     })
-    //console.log(notifications);
-    return apiData;
-    
-}
+import {useLocation,useNavigate,Redirect} from 'react-router-dom';
+import HandleClick from './HandleClick';
+import NavigationButton from './NavigationButton';
 
 function Details() {
     const {state} = useLocation();
+    const navigate = useNavigate();
     const[applicableData, setApplicableData] = useState(null);
-    //getResponse("waiting","12",state, setApplicableData);
-    const[Box3,setBox3] = useState(false);
-    const[Box2, setBox2] = useState(true);
-    
-    const[email, setEmail] = useState(null);
-    const[password, setPassword] = useState(null);
-    let[data, setData] = useState(null);
-    const handleClick = (newData,Box2,Box3) => {
-      setBox2(Box2 = false)
-      setBox3(Box3 = true)
-      setData(data = newData)
-    }
+    const[data , setData] = useState(null);
     const filter = "waiting";
     const first = "12"; 
     useEffect(() => {
@@ -55,119 +19,40 @@ function Details() {
       setApplicableData(apiData);
      })
     }, []);
-    if (applicableData!==null){
-        console.log(applicableData[0]);
-    }
     let trueNotification = null;
     let newVar = null;
-    if (data !==null && applicableData!==null) {
-        const temp = JSON.stringify(applicableData[data].content);
-        newVar = temp;
+    const dataPrinted = [];
+    if (applicableData!==null){
+      for (let i=0;i<applicableData.length;i++){
+        dataPrinted.push(i);
+      }
     }
-    
-    
+    if (data !==null && applicableData!==null) {
+        navigate('/notification',{state:{numberNotification:data,entireData:applicableData, token:state}})
+    }
 
     
     return (
       
       <div className="App">
-        <div className="App-box1">
-          <img src={CodecareIcon} width="200" height="80"/>
-          <p>{state.token}</p>
-          <div className = "lowerContainer">
-          <div className= "leftInnerContainer">
-            <div className = "navButs">
-              <button>
-                <img src = {OverviewIcon} alt = "overview"/>
-                Overview
-              </button>
-              <button>
-                <img src = {NotificationIcon} alt = "notifications"/>
-                Notifications
-              </button>
-              <button>
-                <img src = {MessagesIcon} alt = "messages"/>
-                Messages
-              </button>
-              <button>
-                <img src = {MapIcon} alt = "map"/>
-                Map
-              </button>
-              <button>
-                <img src = {PatientsIcon} alt = "patients"/>
-                Patients
-              </button>
-              <button>
-                <img src = {SettingsIcon} alt = "settings"/>
-                Settings
-              </button>
-            </div>
-          </div>
-        </div>
-        </div>
-          {Box2 && <div className="App-box2">
-            <h1> Current Notifications</h1>
-            <br></br>
-            <ul>
-              <li onClick = {()=>handleClick(1,Box3,Box2)}>Summary of the Notification 1</li>
-              <br></br>
-              <li onClick = {()=>handleClick(2,Box3,Box2)}>Summary of the Notification 2</li>
-              <br></br>
-              <li onClick = {()=>handleClick(3,Box3,Box2)}>Summary of the Notification 3</li>
-              <br></br>
-              <li onClick = {()=>handleClick(4,Box3,Box2)}>Summary of the Notification 4</li>
-              <br></br>
-              <li onClick = {()=>handleClick(5,Box3,Box2)}>Summary of the Notification 5</li>
-              <br></br>
-              <li onClick = {()=>handleClick(6,Box3,Box2)}>Summary of the Notification 6</li>
-              <br></br>
-              <li onClick = {()=>handleClick(7,Box3,Box2)}>Summary of the Notification 7</li>
-              <br></br>
-              <li onClick = {()=>handleClick(8,Box3,Box2)}>Summary of the Notification 8</li>
-              <br></br>
-              <li onClick = {()=>handleClick(9,Box3,Box2)}>Summary of the Notification 9</li>
-              <br></br>
-              <li onClick = {()=>handleClick(10,Box3,Box2)}>Summary of the Notification 10</li>
-              <br></br>
-              <li onClick = {()=>handleClick(11,Box3,Box2)}>Summary of the Notification 11</li>
-              <br></br>
-              <li onClick = {()=>handleClick(12,Box3,Box2)}>Summary of the Notification 12</li>
-              <br></br>
-            </ul>
-          </div>
+        {// following code is the left section line 62
+          <div></div>
         }
-        {newVar !== null &&!Box2 && <Notification data = {data} newVar = {newVar}/> }
-        {Box3 &&<div className="App-box3">
-              <h1> Current Notifications</h1>
-              <ul>
-              <li onClick = {()=>handleClick(1,Box3,Box2)}>Summary of the Notification 1</li>
-              <br></br>
-              <li onClick = {()=>handleClick(2,Box3,Box2)}>Summary of the Notification 2</li>
-              <br></br>
-              <li onClick = {()=>handleClick(3,Box3,Box2)}>Summary of the Notification 3</li>
-              <br></br>
-              <li onClick = {()=>handleClick(4,Box3,Box2)}>Summary of the Notification 4</li>
-              <br></br>
-              <li onClick = {()=>handleClick(5,Box3,Box2)}>Summary of the Notification 5</li>
-              <br></br>
-              <li onClick = {()=>handleClick(6,Box3,Box2)}>Summary of the Notification 6</li>
-              <br></br>
-              <li onClick = {()=>handleClick(7,Box3,Box2)}>Summary of the Notification 7</li>
-              <br></br>
-              <li onClick = {()=>handleClick(8,Box3,Box2)}>Summary of the Notification 8</li>
-              <br></br>
-              <li onClick = {()=>handleClick(9,Box3,Box2)}>Summary of the Notification 9</li>
-              <br></br>
-              <li onClick = {()=>handleClick(10,Box3,Box2)}>Summary of the Notification 10</li>
-              <br></br>
-              <li onClick = {()=>handleClick(11,Box3,Box2)}>Summary of the Notification 11</li>
-              <br></br>
-              <li onClick = {()=>handleClick(12,Box3,Box2)}>Summary of the Notification 12</li>
-              <br></br>
-            </ul>
-          </div>
+        
+        <NavigationButton state = {state}/>
+         
+        
+        {// following code is the middle section lines 68 - 74
         }
-  
+        <div>
+          {applicableData!==null && data === null && <div className="App-box2">
+            
+            <HandleClick updateNumber = {setData} number = {applicableData.length}/>
+          </div>
+          }
+        </div>
+        {// following code is the right section lines 76 - 82 
+        }
       </div>
     );
   }
--- Frontend/codecare-app/src/Notification.js
+++ Frontend/codecare-app/src/Notification.js
@@ -1,24 +1,63 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import NavigationButton from './NavigationButton';
+import { useLocation,useNavigate } from 'react-router-dom';
 import './Notification.css';
-const Notification = (props) => {
-  console.log("Hi nishit"+props.data);
+import HandleClick from './HandleClick';
+function Notification (props) {
+  const{state} = useLocation();
+  const navigate = useNavigate();
+  const[data,setData] = useState(state.numberNotification);
+  const[apiAccept,setApiAccept] = useState(null);
+  const[apiDecline,setApiDecline] = useState(null);
+  useEffect(() => {
+    if(apiAccept!==null){
+      fetch('/send/'+apiAccept+'?token='+state.token)
+      .then(()=>{
+        navigate('/details',{state:state.token})
+      })
+    }
+  }, [apiAccept]);
+  useEffect(() => {
+    if(apiDecline!==null){
+      fetch('/send/'+apiDecline+'?token='+state.token)
+      .then(()=>{
+        navigate('/details',{state:state.token})
+      })
+    }
+  }, [apiDecline]);
+  console.log("Notification number"+state.numberNotification);
+  //console.log(state.entireData);
   return (
-    <div className='Notification'>
-        <div className='Box1'>
-            <h1>Summary and Title of the Selected Notification {props.data.data}</h1>
-            <br></br>
-            <h2>Placeholder Data</h2>
-        </div>
-        <div className='Box2'>
-            <h1>Preview of the message to be sent</h1>
-            <br></br>
-            <p>{props.newVar}</p>
-        </div>
-        <div className='Box3'>
-            <h1>Susceptible Patients</h1>
-            <br></br>
-            <h2>Placeholder Data</h2>
-        </div>
+    <div>
+      {
+         <NavigationButton state = {state.token}/>
+      }
+      {// Lines 18-34 are middle part of the notification. 
+      }
+      <div className='Notification'>
+          <div className='Box1'>
+              <h1>Summary and Title of the Selected Notification {data+1} </h1>
+              <br></br>
+              <h2>Placeholder Data</h2>
+          </div>
+          <div className='Box2'>
+              <h1>Preview of the message to be sent</h1>
+              <br></br>
+              <p>{JSON.stringify(state.entireData[data].content)}</p>
+              <button onClick={()=>setApiAccept(state.entireData[data].notification_id)} type='button'>Send Notification</button>
+              <button onClick={()=>setApiDecline(state.entireData[data].notification_id)} type='button'>Reject Notification</button>
+          </div>
+          <div className='Box3'>
+              <h1>Susceptible Patients</h1>
+              <br></br>
+              <h2>Placeholder Data</h2>
+          </div>
+      </div>
+      {// Lines 37-39 are the right portion of the notification tab.
+      }
+      <div>
+        <HandleClick updateNumber = {setData} number = {state.entireData.length}/>
+      </div>
     </div>
   )
 };
